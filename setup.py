6ed89e5c12ccc0d10af692c6a6aa18876379c36f
Kisuk Lee <kisuklee@mit.edu>
2018-01-11 22:27:32 -0500

    makefile update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     name='datatools',
     version='0.0.1',
     description='A C++ extension to DataProvider.',
-    url='https://github.com/torms3/DataProviderExtension',
+    url='https://github.com/torms3/DataTools',
     author='Kisuk Lee',
     author_email='kisuklee@mit.edu',
     license='MIT',
